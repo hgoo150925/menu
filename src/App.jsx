c77bd02e2276e6725fe680f81b81177252d0fd23
hugoorlando <hugo.orlando2030@gmail.com>
2022-09-26 15:54:03 -0300

    agregando categorias

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import { useState } from 'react';
 
+import { Categories } from './Categories';
+import { Menu } from './Menu';
+
+import { menu } from './data';
+
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState([]);
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories />
+        <Menu menu={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,3 +1,20 @@
-export const Menu = () => {
-  return <div>Menu</div>;
+export const Menu = ({ menu }) => {
+  return (
+    <div className="section-center">
+      {menu.map(({ id, title, price, img, desc }) => {
+        return (
+          <article key={id} className="menu-item">
+            <img className="photo" src={img} alt={title} />
+            <div className="item-info">
+              <header>
+                <h4>{title}</h4>
+                <h4 className="price">{price}</h4>
+              </header>
+              <p className="item-text">{desc}</p>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
 };
